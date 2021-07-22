ed115f14c4b85a45f04239d1da4e77b9e66433e7
Joshua Taillon <joshua.taillon@nist.gov>
2021-07-22 15:12:58 -0400

    Add script to update timestamps of Mongo documents to match XML dates

---
+++ deploy/docker_update_timestamps.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+source .env
+
+docker exec -i "${PROJECT_NAME}_cdcs" python - <<'EOF'
+# needs to run from /srv/curator
+from mdcs import settings   # sets up DB connection
+from core_main_app.components.data import models  # the model from ORM we want
+from datetime import datetime
+import xml.etree.ElementTree as ET
+
+# each Data object has fields such as title, xml_file, creation_date,
+#  last_modification_date, last_change_date, etc. as defined in 
+#  core_main_app/components/abstract_data/models.py 
+#  these can be changed by reading the Document, directly modifying 
+#  `last_change_date` (or whatever) and then running Document.save() to 
+#  commit it to the DB
+for i, doc in enumerate(models.Data.get_all('date')):
+  e = ET.fromstring(doc.xml_content)
+  time = e.find("acquisitionActivity/startTime")
+  if time is None:
+    continue
+  dt = datetime.fromisoformat(time.text)
+  print(f"Updating document {i+1} to {dt}")
+  doc.creation_date = dt
+  doc.last_modification_date = dt
+  doc.last_change_date = dt
+  doc.save()
+EOF
