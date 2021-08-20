e8f8f1c8102ad694c6e34fb3c5b48c3c77a43e6b
Joshua Taillon <joshua.taillon@nist.gov>
2021-08-20 12:39:53 -0400

    Update timestamp updater to better localize times

--- deploy/docker_update_timestamps.sh
+++ deploy/docker_update_timestamps.sh
@@ -8,6 +8,7 @@ from mdcs import settings   # sets up DB connection
 from core_main_app.components.data import models  # the model from ORM we want
 from datetime import datetime
 import xml.etree.ElementTree as ET
+import pytz
 
 # each Data object has fields such as title, xml_file, creation_date,
 #  last_modification_date, last_change_date, etc. as defined in 
@@ -22,6 +23,7 @@ for i, doc in enumerate(models.Data.get_all('date')):
     continue
   dt = datetime.fromisoformat(time.text)
   print(f"Updating document {i+1} to {dt}")
+  dt = pytz.timezone('America/New_York').localize(dt).astimezone(pytz.UTC)  
   doc.creation_date = dt
   doc.last_modification_date = dt
   doc.last_change_date = dt
